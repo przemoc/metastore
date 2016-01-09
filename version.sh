d534dd28eb3f02120087b127bb7738233a752a31
Przemyslaw Pawelczyk <przemoc@gmail.com>
2016-01-09 01:08:02 +0100

    version.sh: Fix sed error on BSDs: bad flag in substitute command: '}'.
    Explicitly terminate last function (s) within list of functions ({...}).

--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@ METASTORE_VER="$(\
 { git describe --tags 2>/dev/null \
   || grep -no '^v[0-9][^ ]*' NEWS \
   || echo $METASTORE_PRETAG_VER; \
-} | sed '/^1:/s,,,;/:/{s,[^:]*:,,;s,$,+,};q' \
+} | sed '/^1:/s,,,;/:/{s,[^:]*:,,;s,$,+,;};q' \
 )"
 
 echo "$METASTORE_VER_PREFIX$METASTORE_VER$METASTORE_VER_SUFFIX"
